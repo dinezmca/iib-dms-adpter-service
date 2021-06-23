8fd8b663b6fc07a9591139406d9db6828857db02
Tamizhini <Tamizhini@DESKTOP-LG6TQB6>
2021-06-23 17:20:13 +0530

    iib dms

---
+++ src/main/java/com/fab/integration/iibdmsadpterservice/IibDmsAdpterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fab.integration.iibdmsadpterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IibDmsAdpterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IibDmsAdpterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fab/integration/iibdmsadpterservice/IibDmsAdpterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fab.integration.iibdmsadpterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IibDmsAdpterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
